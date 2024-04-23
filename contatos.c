0dc66dd4631cbcd9382ad82db07b89082f6c29a7
Onabe142 <160777855+Onabe142@users.noreply.github.com>
2024-04-23 20:31:41 +0000

    Função para adicionar contatos

---
+++ contatos.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#include "contatos.h"
+
+void Adicionar(Contato contatos[], int *pos){
+  printf("Nome: ");
+  fgets(contatos[*pos].nome, 50, stdin);
+  clearBuffer();
+  printf("Sobrenome: ");
+  fgets(contatos[*pos].sobrenome, 50, stdin);
+  printf("Email: ");
+  fgets(contatos[*pos].email, 50, stdin);
+  printf("Telefone: ");
+  scanf("%d", &contatos[*pos].telefone);
+  *pos = *pos + 1;
+  
+}
+
+void clearBuffer(){
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF);
+}
+
+
---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "contatos.h"
+
+int main() {
+  Contato contatos[total];
+  int pos;
+  int opcao;
+  
+  do{
+    printf("1. Adicionar contato\n");
+    printf("2. Deletar contato\n");
+    printf("3. Listar contatos\n");
+    printf("0. Sair\n");
+    printf("Insira a opcao desejada: ");
+    scanf("%d", &opcao);
+    if (opcao == 1){
+      Adicionar(contatos, &pos);
+    }
+  } while(opcao != 0);
+  
+}
