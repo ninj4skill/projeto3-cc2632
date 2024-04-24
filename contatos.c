953d07882eea7f8ebc37698b91e56f18c874864b
Marcelo Boalento <148659100+ninj4skill@users.noreply.github.com>
2024-04-24 18:36:27 +0000

    Atualizada a função Listar

--- contatos.c
+++ contatos.c
@@ -1,22 +1,33 @@
 #include <stdio.h>
 #include <string.h>
 #include "contatos.h"
-
 void Adicionar(Contato contatos[], int *pos){
   if(*pos >= total){
     printf("Limite de contatos atingido\n");
     return;
   }
   printf("Nome: ");
-  fgets(contatos[*pos].nome, 50, stdin);
   clearBuffer();
+  fgets(contatos[*pos].nome, sizeof(contatos[*pos].nome), stdin);
+  contatos[*pos].nome[strcspn(contatos[*pos].nome, "\n")] = '\0';
+  
   printf("Sobrenome: ");
   fgets(contatos[*pos].sobrenome, 50, stdin);
+  contatos[*pos].sobrenome[strcspn(contatos[*pos].sobrenome, "\n")] = '\0';
+  
+
   printf("Email: ");
   fgets(contatos[*pos].email, 50, stdin);
+  contatos[*pos].email[strcspn(contatos[*pos].email, "\n")] = '\0';
+  
   printf("Telefone: ");
   scanf("%d", &contatos[*pos].telefone);
+  
+  clearBuffer();
+
   *pos = *pos + 1;
+
+  
   
 }
 
@@ -39,6 +50,14 @@ void Deletar(Contato contatos[], int *pos){
   *pos = *pos - 1;
 }
 
+void Listar(Contato contatos[], int *pos){
+  for (int i = 0; i < *pos; i++){
+    printf("Nome: %s\t", contatos[i].nome);
+    printf("Sobrenome: %s\t", contatos[i].sobrenome);
+    printf("Email: %s\t", contatos[i].email);
+    printf("Telefone: %d\n", contatos[i].telefone);
+  }
+}
 
 void clearBuffer(){
     int c;
